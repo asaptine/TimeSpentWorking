33e84dac3072ee6fab906dd95c252b5e4c9f590f
Valentin <tin_stuban@hotmail.com>
2021-08-10 16:33:05 +0200

    added calculation to app

---
+++ app.js
@@ -0,0 +1,45 @@
+const submitBtn = document.querySelector("button");
+const DEFAULT_HOURS_OF_WORK = 8;
+const userInputs = document.querySelectorAll("input");
+
+const calculate = () => {
+
+  const payment = userInputs[0].value;
+  const workingDays = userInputs[1].value;
+  const amount = userInputs[2].value;
+
+  if (payment < 4094) {
+    return alert(
+      "Premala plaća! Unesite višu plaću od minimalne plaće u Hrvatskoj!(4094 HRK)"
+    );
+  }
+
+  const oneWorkingDay = payment / workingDays;
+  const hourlyRate = oneWorkingDay / DEFAULT_HOURS_OF_WORK;
+  const hourWorkedAmount = (amount / hourlyRate).toFixed(2);
+  const daysWorkedAmount = hourWorkedAmount / DEFAULT_HOURS_OF_WORK;
+
+  return daysWorkedAmount;
+};
+
+const clearInput= () => {
+  userInputs[0].value = '';
+  userInputs[1].value = '';
+  userInputs[2].value = '';
+}
+
+const render = (daysAmount) => {
+  const h2 = document.querySelector(".result-amount");
+  const daysAmountString = daysAmount.toFixed(2).toString();
+  const daysArray = daysAmountString.split(".");
+  const hourPercentage = DEFAULT_HOURS_OF_WORK * (daysArray[1] / 100);
+  h2.textContent = daysArray[0] + " dana " + hourPercentage + " sati";
+};
+
+submitBtn.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const daysWorkedAmount = calculate();
+  render(daysWorkedAmount);
+
+  clearInput();
+});
